ef9bd0111276ca5bdd636203996ce4e55f500740
Dan Rusei <dan.rusei@gmail.com>
2024-08-25 17:00:38 +0300

    hey is moved to local bin

--- script/loop_requests.sh
+++ script/loop_requests.sh
@@ -4,17 +4,17 @@
 while true
 do
   # Invoke the string producer
-  ./hey_linux_amd64 -n 10 -c 5 -m PUT -H "Content-Type: text/plain" -d "Hello, Danube!" http://localhost:4040/pubsub/string
+  hey -n 10 -c 5 -m PUT -H "Content-Type: text/plain" -d "Hello, Danube!" http://localhost:4040/pubsub/string
   # Wait for 2 seconds
   sleep 2
   
   # Invoke the number producer
-  ./hey_linux_amd64 -n 10 -c 5 -m PUT -H "Content-Type: text/plain" -d "3333" http://localhost:4040/pubsub/number
+  hey -n 10 -c 5 -m PUT -H "Content-Type: text/plain" -d "3333" http://localhost:4040/pubsub/number
   # Wait for 2 seconds
   sleep 2
 
   # Invoke the json producer
-  ./hey_linux_amd64 -n 10 -c 5 -m PUT -H "Content-Type: application/json" -d "{\"field1\": \"example_danube_json\", \"field2\": 123}" http://localhost:4040/pubsub/json
+  hey -n 10 -c 5 -m PUT -H "Content-Type: application/json" -d "{\"field1\": \"example_danube_json\", \"field2\": 123}" http://localhost:4040/pubsub/json
   # Wait for 2 seconds
   sleep 2
 done
